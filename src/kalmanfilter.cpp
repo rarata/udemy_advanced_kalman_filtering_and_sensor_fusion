c605fa76604bed7aa2f98f8da03fd32cfb06c8a8
Ryan Arata <ryanparata@gmail.com>
2023-12-30 21:19:59 -0700

    completed lkf section

--- src/kalmanfilter.cpp
+++ src/kalmanfilter.cpp
@@ -11,10 +11,10 @@
 
 // -------------------------------------------------- //
 // YOU CAN USE AND MODIFY THESE CONSTANTS HERE
-constexpr bool INIT_ON_FIRST_PREDICTION = true;
+constexpr bool INIT_ON_FIRST_PREDICTION = false;
 constexpr double INIT_POS_STD = 0;
-constexpr double INIT_VEL_STD = 15;
-constexpr double ACCEL_STD = 0.1;
+constexpr double INIT_VEL_STD = 5*cos(M_PI_4);
+constexpr double ACCEL_STD = 0.5;
 constexpr double GPS_POS_STD = 3.0;
 // -------------------------------------------------- //
 
@@ -30,15 +30,19 @@ void KalmanFilter::predictionStep(double dt)
         // Hint: You can use the constants: INIT_POS_STD, INIT_VEL_STD
         // ----------------------------------------------------------------------- //
         // ENTER YOUR CODE HERE
-            VectorXd state = Vector4d::Zero();
-            MatrixXd cov = Matrix4d::Zero();
+        VectorXd state = Vector4d::Zero();
+        MatrixXd cov = Matrix4d::Zero();
 
-            // Assume the initial position is (X,Y) = (0,0) m
-            // Assume the initial velocity is 5 m/s at 45 degrees (VX,VY) = (5*cos(45deg),5*sin(45deg)) m/s
-            state << 0, 0, 5.0*cos(M_PI/4), 5.0*sin(M_PI/4);
+        // Assume the initial position is (X,Y) = (0,0) m
+        // Assume the initial velocity is 5 m/s at 45 degrees (VX,VY) = (5*cos(45deg),5*sin(45deg)) m/s
+        state << 0, 0, 5*cos(M_PI_4), 5*cos(M_PI_4);
+        cov(0,0) = INIT_POS_STD*INIT_POS_STD;
+        cov(1,1) = INIT_POS_STD*INIT_POS_STD;
+        cov(2,2) = INIT_VEL_STD*INIT_VEL_STD;
+        cov(3,3) = INIT_VEL_STD*INIT_VEL_STD;
 
-            setState(state);
-            setCovariance(cov);
+        setState(state);
+        setCovariance(cov);
         // ----------------------------------------------------------------------- //
     }
 
@@ -53,6 +57,19 @@ void KalmanFilter::predictionStep(double dt)
         // ----------------------------------------------------------------------- //
         // ENTER YOUR CODE HERE
 
+        // initialize matrices
+        MatrixXd F = Matrix4d(); // F = process model
+        MatrixXd Q = Matrix2d(); // Q = process noise
+        MatrixXd L = MatrixXd(4,2); // L = noise sensitivity
+
+        // fill matrices
+        F << 1,0,dt,0, 0,1,0,dt, 0,0,1,0, 0,0,0,1;
+        Q << ACCEL_STD*ACCEL_STD,0, 0,ACCEL_STD*ACCEL_STD;
+        L << 0.5*dt*dt,0, 0,0.5*dt*dt, dt,0, 0,dt;
+
+        // perform prediction
+        state = F*state;
+        cov = F*cov*F.transpose() + L*Q*L.transpose();
 
         // ----------------------------------------------------------------------- //
 
@@ -75,7 +92,24 @@ void KalmanFilter::handleGPSMeasurement(GPSMeasurement meas)
         // ----------------------------------------------------------------------- //
         // ENTER YOUR CODE HERE 
 
+        // initialize matrices
+        VectorXd z = Vector2d();
+        MatrixXd H = MatrixXd(2,4); // H = measurement matrix
+        MatrixXd R = Matrix2d(); // R = measurement uncertainty covariance matrix
 
+        // fill matrices
+        z << meas.x, meas.y;
+        H << 1,0,0,0, 0,1,0,0;
+        R << GPS_POS_STD*GPS_POS_STD,0, 0,GPS_POS_STD*GPS_POS_STD;
+
+        // intermediate calculations
+        Vector2d y = z - H*state; // y = innovation
+        MatrixXd S = H*cov*H.transpose() + R;
+        MatrixXd K = cov*H.transpose()*S.inverse();
+        
+        // perform update step
+        state = state + K*y;
+        cov = (Matrix4d::Identity() - K*H)*cov;
         // ----------------------------------------------------------------------- //
 
         setState(state);
@@ -92,6 +126,13 @@ void KalmanFilter::handleGPSMeasurement(GPSMeasurement meas)
         // ENTER YOUR CODE HERE
             VectorXd state = Vector4d::Zero();
             MatrixXd cov = Matrix4d::Zero();
+
+            state(0) = meas.x;
+            state(1) = meas.y;
+            cov(0,0) = GPS_POS_STD*GPS_POS_STD;
+            cov(1,1) = GPS_POS_STD*GPS_POS_STD;
+            cov(2,2) = INIT_VEL_STD*INIT_VEL_STD;
+            cov(3,3) = INIT_VEL_STD*INIT_VEL_STD;
 
 
             setState(state);
